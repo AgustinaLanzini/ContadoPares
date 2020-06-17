00128b221e0cac1f2513285349dce5f3aa8e75f5
unknown <agustinalanzini@gmail.com>
2020-06-17 18:25:13 -0300

    Esqueleto de tests

---
+++ src/main/java/org/example/ContadorPares.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class ContadorPares {
+
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+}
--- src/main/java/tallerjava.lanzini/Main.java
+++
@@ -1,9 +0,0 @@
-package edu.unicen.tallerjava;
-
-public class Main
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
